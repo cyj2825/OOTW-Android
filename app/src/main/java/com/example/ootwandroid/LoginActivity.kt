1c9c5a5c9812484832728731903ba4e0a869ea41
cyj2825 <cyj2825@naver.com>
2022-04-23 15:23:05 +0900

    로그인 화면 구현

--- app/src/main/java/com/example/ootwandroid/LoginActivity.kt
+++ app/src/main/java/com/example/ootwandroid/LoginActivity.kt
@@ -2,18 +2,18 @@ package com.example.ootwandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.ootwandroid.databinding.ActivityMainBinding
+import com.example.ootwandroid.databinding.ActivityLoginBinding
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     // 전역 변수로 바인딩 객체 선언
-    private var mBinding: ActivityMainBinding? = null
+    private var mBinding: ActivityLoginBinding? = null
     // 매번 null 체크를 할 필요없이 편의성을 위해 바인딩 변수 재선언
     private val binding get() = mBinding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         // 바인딩
-        mBinding = ActivityMainBinding.inflate(layoutInflater)
+        mBinding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
     }
 }
